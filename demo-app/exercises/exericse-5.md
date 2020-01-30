6863212ecf52e0a54d8e2c837b655dcc0d88148e
Eric Greene <eric@t4d.io>
2020-01-29 19:25:04 -0500

    add code demo

---
+++ demo-app/exercises/exericse-5.md
@@ -0,0 +1,9 @@
+# Exercise 5
+
+[https://github.com/t4d-classes/react-typescript_01292020](https://github.com/t4d-classes/react-typescript_01292020)
+
+## Steps
+
+1. Add car form to the Car Tool component under the table to collect car data for a new car. Provide input fields for the make, model, year, color and price.
+
+2. Do not add the car to the array, but on each keystroke output the car form object to the console.
--- demo-app/src/components/ColorTool.tsx
+++ demo-app/src/components/ColorTool.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FC, useState } from 'react';
 
 import { Color } from '../models/color';
 
@@ -6,7 +6,30 @@ interface ColorToolProps {
   colors: Color[];
 }
 
-export const ColorTool: React.FC<ColorToolProps> = (props) => {
+interface ColorFormState {
+  name: string;
+  hexcode: string;
+  [ x: string ]: string;
+}
+
+const initialColorForm: () => ColorFormState = () => ({
+  name: '',
+  hexcode: '',
+});
+
+export const ColorTool: FC<ColorToolProps> = (props) => {
+
+  const [ colorForm, setColorForm ] = useState(initialColorForm());
+
+  const change = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setColorForm({
+      ...colorForm,
+      [ e.target.name ]: e.target.value,
+    })
+  };
+
+  console.log('component rendering');
+  console.log(colorForm);
 
   return (
     <>
@@ -16,6 +39,16 @@ export const ColorTool: React.FC<ColorToolProps> = (props) => {
       <ul>
         {props.colors.map(c => <li key={c.id}>{c.name}</li>)}
       </ul>
+      <form>
+        <div>
+          <label htmlFor="name-input">Name</label>
+          <input type="text" id="name-input" value={colorForm.name} name="name" onChange={change} />
+        </div>
+        <div>
+          <label htmlFor="hexcode-input">Hexcode</label>
+          <input type="text" id="hexcode-input" value={colorForm.hexcode} name="hexcode" onChange={change} />
+        </div>
+      </form>
     </>
   );
 
